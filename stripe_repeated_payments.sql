e75523bff2b030045fea53e4fb7a47e093171ebb
hali02 <127011409+hali02@users.noreply.github.com>
2023-03-05 02:49:58 -0500

    Create stripe_repeated_payments.sql

---
+++ stripe_repeated_payments.sql
@@ -0,0 +1,13 @@
+WITH repeats AS
+(SELECT t1.merchant_id, (t1.transaction_timestamp) AS first_trans, t2.transaction_timestamp, t1.amount
+FROM transactions t1
+INNER JOIN transactions t2 
+ON t1.transaction_timestamp != t2.transaction_timestamp
+AND t1.merchant_id = t2.merchant_id
+AND t1.amount = t2.amount
+AND t1.credit_card_id = t2.credit_card_id
+WHERE t1.transaction_timestamp < t2.transaction_timestamp
+AND t2.transaction_timestamp <= (t1.transaction_timestamp + interval '10 minutes'))
+
+SELECT COUNT(first_trans)
+FROM repeats
